eb22da93cca658cdaa69f93544731e29a4e4906d
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2022-11-05 00:59:11 +0300

    done

---
+++ JS/DataTypes.js
@@ -0,0 +1,44 @@
+"use strict";
+/* Datatypes ku waa kuwa ugu muhiimsan dhammaan programming langauges ka
+waana kuwa jaan gooya waxkasta oo la kaydinayo sidoo kale programm kaas xalinayo
+hadaba waxa jira programming languages ka aan type ka oo la cadeeyo la,aantii aan waxba qaban karin
+waxana kamid ah Typescript waxana ay caksi ku tahay Luuqada laga dul dhisay ee JavaScript
+
+Typescript waxay isticmaasha Dhamaan Types ka jira ee programming langauges ku leeyihiin waxana jira waxyaabo kale oo ay kaga duwan tahay luuqadaha qaar kood kuwas oo aan arki doono insha allah
+marka aynu gaadhno
+
+hadaba Types ka laba qaybood ayaa loo qaybiyaa marka Typescript la joogo
+
+// Primitive-Types = waana kuwa asalka ah ee laf dhabarka ah
+1. string
+2. number
+3. boolean
+4. null
+5. undefined
+
+// none-PrimitiveTypes = kuwan waa khiyaar mararka qaar aanad isticmaalaynin baahidoodo joogto maaha
+
+1. array
+2. objects
+*/
+// Primitive-Types = waana kuwa asalka ah ee laf dhabarka ah
+// 1. string 
+let myNames = "Mohamed Ali";
+console.log(myNames);
+// 2. number
+let myNumbers = 44444;
+console.log(myNumbers);
+// 3. boolean 
+let isStudent = true;
+console.log(isStudent);
+// 4. null
+let isNull = null;
+console.log(isNull);
+// 5. undefined
+let isUndefined;
+console.log(isUndefined);
+// hadaba si aynu Typeka ugu soo muujino console ka ama meshi kaleba 
+// waxaynu isticmaalaynaa 
+console.log(typeof myNames);
+// sidaas oo kale ayad variable kasta typekiisa kusoo saari karta 
+// hadii aad rabto inaad ka ogaato browser ka
