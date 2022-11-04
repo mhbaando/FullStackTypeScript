b756b08840fca1bc31a6e680653bdbd5c2980901
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2022-11-05 01:40:45 +0300

    Done

---
+++ JS/Arrays.js
@@ -0,0 +1,38 @@
+"use strict";
+/*
+Arrays: waa nooc kamid ah Objects waxana lagu soo bandhigaa values badan oo kaa qaadan lahaa variables badan
+hadaba si aynu u fahano aya waxa lagu idhi waxaad soo daabacdaa ama aad soo saartaa
+tusaale ahaan dad faro badan magac yadoodii waxana lagu idhi hal meel haaka wada imaadaan
+
+labadan tusaale kii adiga kuu fudud waad garan karaysaa aan isla eegno:
+*/
+// kan hore :
+let magaca1 = "Cismaan maxamed";
+let magaca2 = "Caasha daahir";
+let magaca3 = "Warsame";
+let magaca4 = "Ayaan";
+// console.log(magaca1)
+// console.log(magaca2)
+// console.log(magaca3)
+// console.log(magaca4)
+// kan Dambe:
+let Magacyo = ['Cismaan Maxamed', 'Caasha Daahir', 'Warsame', 'Ayaan'];
+console.log(Magacyo);
+// hadaba sidan hore iyo sidan dambe kee aya fudud 
+// midkalena meesha waa ay taalaa oo ah in variable kasta oo aad samayso 
+// memoryga computer kaaga ayaa meel lagaga kaydinaya si ku meel gaadh ah 
+// hadii kawaran aad buuxiso o aad faro badiso iyadoo aad soo koobi karayso wixii lagaa doonaayya 
+// memory giina anad culays ku ridin
+// hadaba Arraysku wuu leeyahay method ama properties kala duwan kuwaas oo ay shaqoodu tahay in aad array gii ku maamuli karto tusaale ahaan markaad rabto inaad mid mid gaar ula soo bixi karto inaad mid kale kusii dari karto adoon arrays ka toos u dhex tagin iyo sidoo kale inaad ka saari karto sidoo kale waxa jira mid kale oo la idhaah Destructuring arrays oo isna inoosoo socda insha allah 
+// hadaba an bilowno 
+// markaad lasoo baxayso mid kamida magacyada kan ugu horeya oo kale waxa uu ka bilaabmayaa godka siiroo 0,1,2,3,4,5,6,7,8,9 waa sidaas sida uu arrays u shaqeeyo 
+console.log(Magacyo[0]);
+// markaad mid ku darayso
+// magaca samiira magacyada hore kuma jira lkn markaad soo daabacdo wuu soo racaya waxana suurto galiyay method kan push la idhaah
+console.log(Magacyo.push('samiira'));
+// markad ka saarayso mid xagga dambe 
+console.log(Magacyo.shift());
+// waxa lamid ah unShift
+console.log(Magacyo.unshift());
+// waxa lamid ah Slice
+console.log(Magacyo.slice());
