c2924fa355683ce0d832c9a86ac8fa5c406150ed
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2022-11-04 23:59:02 +0300

    done:

---
+++ TS/Variable.ts
@@ -0,0 +1,15 @@
+// laba hab oo kala duwan ayaa loo samaystaa Variable ka marka la joogo Typescript
+
+// midka hore waxa loo yaqaan Declaration Variable 
+// oo ah mid aad sheegi karto Typeka iyo magaca Variable ka 
+// kadib ayad Qiimaha Gaar u siinaysaa 
+
+let fariinQoraaled:string;
+fariinQoraaled = "kusoo dhawoow Typescript !";
+console.log(fariinQoraaled);
+
+// habka kale waxa loo yaqaan Initialization Variable 
+// oo ah mid ad iskugu raacinayso dhamaan 
+// Typeka Variable ka Value ga 
+const fariinQoraaled1:string = "Ahleen TypeScript";
+
