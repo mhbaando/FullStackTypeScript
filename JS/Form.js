8558dab7f704c41c974573090b7e0c3c7f38cfb3
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2022-11-05 03:38:44 +0300

    Done

--- JS/Form.js
+++ JS/Form.js
@@ -1,5 +1,9 @@
 "use strict";
 const Close = document.querySelector('.Close');
+const Send = document.querySelector('.send');
 Close === null || Close === void 0 ? void 0 : Close.addEventListener('click', () => {
     document.body.remove();
+});
+Send === null || Send === void 0 ? void 0 : Send.addEventListener('click', () => {
+    alert("waad ku mahadsantahay Waala Diray");
 });
--- TS/Form.ts
+++ TS/Form.ts
@@ -1,5 +1,10 @@
 const Close = document.querySelector('.Close');
+const Send  = document.querySelector('.send');
 
 Close?.addEventListener('click',()=>{
 document.body.remove();
 });
+
+Send?.addEventListener('click',()=>{
+    alert("waad ku mahadsantahay Waala Diray")
+})
